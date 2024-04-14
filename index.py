e03bbb5b54e8e61aa82c12b5d2705f6ac820b3f7
bit-barron <1bit.baron@gmail.com>
2024-04-14 11:55:51 +0200

    asdasd

--- index.py
+++ index.py
@@ -9,7 +9,7 @@ BASE_URL = "https://gemini.discord.rocks/vision"
 
 @app.route('/')
 def home():
-    return '<h1>Hello Worldasdasdasdasd asd</h1>'
+    return '<h1>Hellso Worldasdasdasdasdasd asd</h1>'
 
 @app.route('/image/vision', methods=['POST'])
 def vision_endpoint():
